81389ac652078c8d9142596e886e3d6e21e8f952
josmas <josmasflores@gmail.com>
2016-05-17 20:41:58 +0100

    Initialisation of client object

--- test/unit/client.js
+++ test/unit/client.js
@@ -1,9 +1,14 @@
 'use strict';
 
-var client = require('../../index');
+var client = require('../../index')({ username: "jos", password: "jos"});
+var unInitialisedClient = require('../../index');
 var expect = require('chai').expect;
 
 describe('A client instance', function(){
+  it('should throw and error for a uninitialised client', function () {
+    expect(unInitialisedClient).to.throw(Error);
+  });
+
   it('should be of type object', function () {
     expect(client).to.be.a('object');
   });
