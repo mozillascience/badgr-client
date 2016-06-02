6780a8fe99196daa658fb587a6bebac6834d0782
drammock <dan.mccloy@gmail.com>
2016-06-02 16:03:17 -0700

    pull from .env in unittests (not just integration tests)

--- test/unit/index.js
+++ test/unit/index.js
@@ -2,7 +2,8 @@
 
 var Index = require('../../index');
 var expect = require('chai').expect;
-var apiEndpoint = 'http://192.168.99.100:8000/'; // TODO (jos) read from .env variable || default
+var env = require('../../src/environments');
+var apiEndpoint = env.get('badgrApiEndpoint');
 
 describe('A library instance', function() {
   it('should return error for a call to the library with no auth data', function () {
