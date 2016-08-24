58d1a3520fd4aa11047c224ac921c26cb8069f4b
josmas <josmasflores@gmail.com>
2016-08-24 12:33:07 +0100

    create a new assertion to issue a badge to a single recipient

---
+++ test/integration/issuing_methods.js
@@ -0,0 +1,32 @@
+/* eslint camelcase: ["error", {properties: "never"}]*/
+'use strict';
+
+var Index = require('../../index');
+var expect = require('chai').expect;
+var env = require('../../src/environments');
+var apiEndpoint = env.get('badgrApiEndpoint');
+var goodTestAuth = { username: env.get('badgrUser'), password: env.get('badgrPass')};
+
+describe('A higher level client using issuing methods', function() {
+
+  it('should create a badge under the issuer passed in', function (done) {
+    var client = new Index(apiEndpoint, goodTestAuth);
+
+    var opts = {
+      path: 'v1/issuer/issuers',
+      issuerSlug: 'mozilla-science',
+      badgeSlug: 'software',
+      recipient_identifier: 'b@d.gr',
+      evidence: 'http://example.com/noevidence',
+      create_notification: 'true'
+    };
+    client.createBadgeInstance(opts, function (err, data) {
+      expect(err).to.be.null;
+      expect(data).not.to.be.undefined;
+      var returnedBadgeSlug = data.json.badge.split('/');
+      expect(returnedBadgeSlug[returnedBadgeSlug.length - 1]).to.equal(opts.badgeSlug);
+      done();
+    });
+  });
+
+});
